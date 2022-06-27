42232edcb9b05ebbe0664374a0dd1d15b9973a23
Aoba Ikushima <78516535+sock8@users.noreply.github.com>
2022-06-27 13:28:49 +1200

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,4 +1,28 @@
-def q2(q1a):
-    if 8
+def q2(more):
+    options = {'causes': "efiohaofshfsohfa", 'effects': "ioawuooidhasyifoejtbibnafuodcn09mjfishiufidsouisuvovfo8ub0eyfasioofhsohigltyuui"}
+    if more == "yes":
+        valid = False
+        while not valid:
+            choice = input("""what would you like to find out more about mental health issues?
+             Type 'c' to find out about the causes and 'e' to find out about the effects.""")
+            if choice == "c":
+                print(options['causes'])
+                break
+            elif choice == "e":
+                print(options['effects'])
+                break
+            else:
+                print("please type c or e")
+    else:
+        return
 
-q2(q1a)
+valid = False
+while not valid:
+    more = input("Would you like to find out more about what mental health issues are? type yes or no")
+    if more == "yes":
+        q2a = q2(more)
+        break
+    elif more == "no":
+        break
+    else:
+        print("try again")
